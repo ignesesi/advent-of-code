fb7c0edd3847c5bed26ece5458ffcc6b8fccec9d
Ignes Simeonova <esiignes@gmail.com>
2023-12-11 21:17:39 +0200

    d10 p1 use struct

--- day10/part1.cpp
+++ day10/part1.cpp
@@ -4,16 +4,18 @@
 #include <set>
 using namespace std;
 
-bool is_in_table(int num, int lim) {
-    return num >= 0 and num < lim;
+struct Cell {
+    int row, col;
+};
+
+bool is_in_table(const vector<string>& table, const Cell& cell) {
+    return cell.row >= 0 and cell.col >= 0 and cell.row < table.size() and cell.col < table[0].size();
 }
 
-pair<int, int> other_ngbr(const vector<string>& table, pair<int, int> curr, pair<int, int> last) {
-    const int ROWS = table.size();
-    const int COLS = table[0].size();
-
+Cell other_ngbr(const vector<string>& table, Cell curr, Cell last) {
+    /// define symbols
     char pipes[7] = "|-LJ7F";
-    pair<int, int> delta_ngbrs[6][2] = {
+    Cell delta_ngbrs[6][2] = {
         {{-1, 0}, {1, 0}},
         {{0, -1}, {0, 1}},
         {{-1, 0}, {0, 1}},
@@ -22,33 +24,29 @@ pair<int, int> other_ngbr(const vector<string>& table, pair<int, int> curr, pair
         {{1, 0},  {0, 1}}
     };
 
-    int row = curr.first;
-    int col = curr.second;
-    int last_row = last.first;
-    int last_col = last.second;
-
     for (int i = 0; i < 6; i++) {
-        if (table[row][col] != pipes[i]) {
+        if (table[curr.row][curr.col] != pipes[i]) {
             continue;
         }
         for (int j = 0; j < 2; j++) {
-            const auto& ngbr = delta_ngbrs[i][j];
-            int n_r = row + ngbr.first;
-            int n_c = col + ngbr.second;
+            const auto& d_n = delta_ngbrs[i][j];
+            Cell ngbr = {curr.row + d_n.row, curr.col + d_n.col};
 
             /// check in table
-            if (!is_in_table(n_r, ROWS) or !is_in_table(n_c, COLS)) {
+            if (!is_in_table(table, ngbr)) {
                 continue;
             }
 
             /// visited
-            if (n_r == last_row and n_c == last_col) {
+            if (ngbr.row == last.row and ngbr.col == last.col) {
                 continue;
             }
 
-            return {n_r, n_c};
+            return ngbr;
         }
     }
+
+    /// ERROR
     cout << "ERROR other_ngbr\n";
     exit(-1);
 }
@@ -57,23 +55,30 @@ int solve(const vector<string>& table) {
     const int ROWS = table.size();
     const int COLS = table[0].size();
 
-    pair<int, int> last, curr;
+    Cell last, curr;
     for (int r = 0; r < ROWS; r++) {
         for (int c = 0; c < COLS; c++) {
             if (table[r][c] == 'S') {
-                pair<int, int> d[4] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+                /// find ngbrs connecte to S
+                Cell d[4] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
                 for (int k = 0; k < 4; k++) {
-                    curr = {r+d[k].first, c+d[k].second};
-                    if (!is_in_table(curr.first, ROWS) or !is_in_table(curr.second, COLS)) {
+                    curr = {r+d[k].row, c+d[k].col};
+
+                    /// check in table
+                    if (!is_in_table(table, curr)) {
                         continue;
                     }
-                    if (table[curr.first][curr.second] == '.') {
+
+                    /// check if ngbr is not a .
+                    if (table[curr.row][curr.col] == '.') {
                         continue;
                     }
+
+                    /// check if ngbr is connected to S
                     last = {r, c};
-                    pair<int, int> ngbr = other_ngbr(table, curr, last);
-                    pair<int, int> start = other_ngbr(table, curr, ngbr);
-                    if (table[start.first][start.second] == 'S') {
+                    Cell ngbr = other_ngbr(table, curr, last);
+                    Cell start = other_ngbr(table, curr, ngbr);
+                    if (table[start.row][start.col] == 'S') {
                         break;
                     }
                 }
@@ -83,9 +88,10 @@ int solve(const vector<string>& table) {
         }
     }
 
+    /// find path
     int ans = 1;
-    while (table[curr.first][curr.second] != 'S') {
-        pair<int, int> ngbr = other_ngbr(table, curr, last);
+    while (table[curr.row][curr.col] != 'S') {
+        Cell ngbr = other_ngbr(table, curr, last);
         last = curr;
         curr = ngbr;
         ans++;
